d17eb3f2e32839d3304412beb9f91d015d1c83fe
Asamaporn Pladsri <jajah.asmp@gmail.com>
2023-12-15 19:17:11 +0700

    [Update] Remove Repositories

--- ShortPaper_API/Repositories/SubjectRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ShortPaper_API.Repositories
-{
-    public class SubjectRepository
-    {
-        public int Id { get; set; }
-        public string StudentId { get; set; }
-        public string SubjectName { get; set; }
-    }
-}
