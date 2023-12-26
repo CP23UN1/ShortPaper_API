1c74f0429cb0481f81cad82e10445378eeb0ada5
smile1346 <armsmile1346@gmail.com>
2023-12-27 00:40:07 +0700

    [Update] fix error of IStudentService

--- ShortPaper_API/Services/Students/IStudentService.cs
+++ ShortPaper_API/Services/Students/IStudentService.cs
@@ -7,7 +7,7 @@ namespace ShortPaper_API.Services.Students
     public interface IStudentService
     {
         ServiceResponse<List<StudentDTO>> GetStudents();
-        StudentDTO GetStudent(string id);
+        ServiceResponse<StudentDTO> GetStudent(string id);
         ServiceResponse<CreateStudentDTO> CreateStudent(CreateStudentDTO studentDTO);
         ServiceResponse<UpdateStudentDTO> UpdateStudent(UpdateStudentDTO studentDTO);
         Student DeleteStudent(string id);
