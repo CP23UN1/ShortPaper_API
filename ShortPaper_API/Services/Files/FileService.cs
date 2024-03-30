c8d10d73910f414aa49bfa9d0da318b0bdcbfedb
smile1346 <armsmile1346@gmail.com>
2024-03-31 04:08:03 +0700

    [Update] edit download file

--- ShortPaper_API/Services/Files/FileService.cs
+++ ShortPaper_API/Services/Files/FileService.cs
@@ -136,7 +136,13 @@ namespace ShortPaper_API.Services.Files
                 return null;
             }
 
-            var filePath = Path.Combine(_hostingEnvironment.WebRootPath, "uploads", latestVersionFile.FileName);
+            // Get the path to the wwwroot directory
+            var webRootPath = _hostingEnvironment.WebRootPath;
+
+            // Construct the path to the uploads directory
+            var uploadsDirectory = Path.Combine(webRootPath, "uploads");
+
+            var filePath = Path.Combine(uploadsDirectory, latestVersionFile.FileName);
 
             var fileBytes = System.IO.File.ReadAllBytes(filePath);
 
