486d1463a1f62e52e0fb0bf9cfde3670820b4b86
smile1346 <armsmile1346@gmail.com>
2024-04-16 00:18:37 +0700

    [Update] re filterArticleDTO

--- ShortPaper_API/DTO/FilterArticleDTO.cs
+++ ShortPaper_API/DTO/FilterArticleDTO.cs
@@ -2,14 +2,14 @@
 {
     public class FilterArticleDTO
     {
-        public string? Topic { get; set; }
-
-        public string? Author { get; set; }
+        public string? TopicOrAuthor { get; set; }
 
         public string? FileName { get; set; }
 
         public string? Year { get; set; }
 
         public string? FileType { get; set; }
+
+        public string? Subject { get; set; }
     }
 }
